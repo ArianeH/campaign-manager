ee85ec8044a83b0497145fb57aa961d12ba0d85e
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-30 10:08:25 +0100

    Iteration over KPIs in campaigns.json and display by adding a Card component and returning a card with KPI values for each campaign

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import campaignsData from './campaigns.json';
 
 class App extends Component {
   render() {
+      var campaigns = {campaignsData};
+      var campaignsArray = [];
+      Object.keys(campaignsData).forEach(function(key) {
+        campaignsArray.push(campaignsData[key])
+      });
+      return (
+        <div className="container">
+          <h1 className="page-heading">Events Analytics </h1>
+          <div>{
+            campaignsArray.map(item =>
+              <Card title={item.title} goal={item.goal} days={item.days}
+              startDate={item.start_date} endDate={item.end_date}
+              reach={item.reach} captureRate={item.capture_rate} views={item.views}
+              interactionRate={item.interaction_rate} interactions={item.interactions}
+              leadConversationAmount={item.lead_conversion_amount}
+              leadConversationValueCents={item.lead_conversion_value_cents}
+              salesConversationAmount={item.sales_conversion_amount}
+              salesConversationValueCents={item.sales_conversion_value_cents} />)
+          }
+          </div>;
+        </div>
+      );
+  }
+}
+
+export class Card extends Component {
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h3>{this.props.title} </h3>
+        <div>Goal: {this.props.goal}</div>
+        <div>Total Amount of Days: {this.props.days}</div>
+        <span>Start Date: {this.props.startDate} </span>
+        <span>End Date: {this.props.endDate}</span>
+        <div>Reach: {this.props.reach}</div>
+        <div>Capture Rate: {this.props.captureRate}</div>
+        <div>Views: {this.props.views}</div>
+        <div>Interaction Rate: {this.props.interactionRate}</div>
+        <div>Interactions: {this.props.interactions}</div>
+        <div>Lead Conversation Amount: {this.props.leadConversationAmount}</div>
+        <div>Lead Conversation Value Cents: {this.props.leadConversationValueCents}</div>
+        <div>Sales Conversation Amount: {this.props.salesConversationAmount}</div>
+        <div>Sales Conversation Value Cents: {this.props.salesConversationValueCents}</div>
       </div>
     );
   }
