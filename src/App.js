fafb1c93c253452a64daaf7e45d5769c4ad00da0
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-12-01 16:10:51 +0100

    Adjusted style sheets to use scss for display of results

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Pagination from './Pagination';
 import PropTypes from 'prop-types';
 import Dropdown, { DropdownTrigger, DropdownContent } from 'react-simple-dropdown';
