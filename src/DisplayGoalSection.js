a8634a06dd66f9b717256ef159c2f5dee3687289
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2018-01-19 10:24:33 +0100

    Refactored if else statement for evaluation of goal definition and simplified the DisplayGoalSection component

--- src/DisplayGoalSection.js
+++ src/DisplayGoalSection.js
@@ -2,42 +2,17 @@ import React, { Component } from 'react';
 import DisplayGoalOptions from './DisplayGoalOptions.js';
 
 export class DisplayGoalSection extends Component {
-  constructor() {
-    super();
-
-    this.state = { value: "select" };
-
-    this.change = this.change.bind(this);
-  }
-
-  change(event) {
-    this.setState({value: event.target.value})
-  }
-
 
   render() {
 
-    return (
-      <div>
-      { this.props.goal !== null &&
-        <div> Goal: {this.props.goal} </div>
-      }
-      { this.props.goal == null &&
-          <DisplayGoalOptions goal={this.props.goal}/>
-      }
+    if (this.props.goal !== null) {
+      return ( <div> Goal: {this.props.goal} </div> )
+    } else {
+      return ( <DisplayGoalOptions goal={this.props.goal}/> )
+    }
 
-      </div>
-    );
   }
 
 }
-
-// function evaluateGoal(goal) {
-//   if (goal !== null) {
-//     return (<div>Goal: {goal}</div>)
-//   } else {
-//     // TODO
-//   }
-// }
 
 export default DisplayGoalSection;
